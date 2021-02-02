bf31ed0aabb694801c4383cb6ac2096d1d9ede39
naoki2001 <66299150+naoki2001@users.noreply.github.com>
2021-02-02 12:49:32 +0900

    Update other.go

--- subcmd/other.go
+++ subcmd/other.go
@@ -2,10 +2,12 @@ package subcmd
 
 import (
 	"fmt"
+	"runtime"
 )
 
 // Version view version of kvm-compose
 func Version() {
-	fmt.Print("kvm-compose version 1.0.0alpha\n")
+	fmt.Print("kvm-compose version: 1.0.0 alpha\n")
+	fmt.Println("go version: ", runtime.Version())
 	return
 }
